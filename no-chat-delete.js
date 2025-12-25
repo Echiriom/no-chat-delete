8e58ae71dbd220c76cf01853c040f04166788b76
Echiriom <brunotanleyandroid@gmail.com>
2025-12-25 14:21:11 -0300

    Update no-chat-delete.js

--- no-chat-delete.js
+++ no-chat-delete.js
@@ -1,9 +1,10 @@
 Hooks.on("getChatLogEntryContext", (html, options) => {
   const deleteOption = options.find(o => o.name === "CHAT.Delete");
 
-  if (deleteOption) {
-    deleteOption.condition = () => {
-      return game.user.isGM;
-    };
-  }
+  if (!deleteOption) return;
+
+  // Só GM pode ver/usar excluir
+  deleteOption.condition = () => {
+    return game.user.isGM;
+  };
 });
